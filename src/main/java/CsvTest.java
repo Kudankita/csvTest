5a4eef64e6367355122e6082a77b25385aeeba92
Silver <41056212+SilverCafe@users.noreply.github.com>
2018-12-29 19:14:58 +0900

    CSVを保存することに成功。保存ファイルのエンコードの挙動を観察できた

--- src/main/java/CsvTest.java
+++ src/main/java/CsvTest.java
@@ -1,10 +1,15 @@
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CsvTest {
+  /**
+   * @param args 利用せず
+   * @throws IOException 対処せず
+   */
   public static void main(String[] args) throws IOException {
     System.out.println(System.getProperty("file.encoding"));
     File file = new File("G:\\study\\IDEA\\csvTest\\src\\main\\resources\\1301_2018.csv");
@@ -23,5 +28,12 @@ public class CsvTest {
       csvLines.add(nextLine);
     }
     reader.close();
+    // ここでファイルを保存するが、特に指定していないので、環境がUTF8ならそれ、sjisならばそれで保存する
+    // ideaで実行するとUTF８で保存され、powershellで保存するとsjisになる
+    CSVWriter writer = new CSVWriter(new FileWriter("yourfile.csv"));
+    for (String[] sList : csvLines) {
+      writer.writeNext(sList);
+    }
+    writer.close();
   }
 }
